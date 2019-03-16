d4ffc269a47ade60e75ec61dbb3d0d9c92bd0dc9
Przemek Zych <zychster@gmail.com>
2019-03-16 22:42:13 +0100

    Tests added

---
+++ src/test/com/ge/edc/devchapter/observerkata/Tests.java
@@ -0,0 +1,39 @@
+package com.ge.edc.devchapter.observerkata;
+
+import com.ge.edc.devchapter.observerkata.interfaces.Subscriber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Tests {
+
+    @Test
+    public void classSmartphoneAppImplementsSubscriber(){
+        SmartphoneApp smartphoneApp = new SmartphoneApp();
+
+        assertTrue(smartphoneApp instanceof Subscriber);
+    }
+
+    @Test
+    public void classTvStripImplementsSubscriber(){
+        TvStrip tvStrip = new TvStrip();
+
+        assertTrue(tvStrip instanceof Subscriber);
+    }
+
+    @Test
+    public void classWebsiteChartImplementsSubscriber(){
+        WebsiteChart websiteChart = new WebsiteChart();
+
+        assertTrue(websiteChart instanceof Subscriber);
+    }
+
+    @Test
+    public void classStockExchangeImplementsSubject(){
+        WebsiteChart websiteChart = new WebsiteChart();
+
+        assertTrue(websiteChart instanceof Subscriber);
+    }
+
+
+}
