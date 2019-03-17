95459452aa3bf771a736c61962e436b483433baf
Przemek Zych <zychster@gmail.com>
2019-03-17 22:42:13 +0100

    Solution to step2 added.

--- src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
+++ src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
@@ -6,16 +6,20 @@ import com.ge.edc.devchapter.observerkata.interfaces.Subscriber;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+
 public class StockExchange implements Subject {
 
     List<Subscriber> subscribers = new ArrayList<>();
 
     @Override
     public void addSubscriber(Subscriber subscriber) {
+        subscribers.add(subscriber);
     }
 
     @Override
     public void removeSubscriber(Subscriber subscriber) {
+        subscribers.remove(subscriber);
     }
 
     @Override
