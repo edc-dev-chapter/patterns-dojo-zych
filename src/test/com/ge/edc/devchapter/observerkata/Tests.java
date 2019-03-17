bf47443377fa98cc2c3e4c90ccabd66e3161d3a3
Przemek Zych <zychster@gmail.com>
2019-03-17 10:33:30 +0100

    Tests to step2 added

--- src/test/com/ge/edc/devchapter/observerkata/Tests.java
+++ src/test/com/ge/edc/devchapter/observerkata/Tests.java
@@ -3,6 +3,11 @@ package com.ge.edc.devchapter.observerkata;
 import com.ge.edc.devchapter.observerkata.interfaces.Subscriber;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Tests {
@@ -35,5 +40,33 @@ class Tests {
         assertTrue(websiteChart instanceof Subscriber);
     }
 
+    @Test
+    public void StockExchangeAcceptsNewSubscribers() {
+        StockExchange stockExchange = new StockExchange();
+        stockExchange.addSubscriber(()->{});
 
+        assertEquals(1, stockExchange.subscribers.size());
+    }
+
+    @Test
+    public void StockExchangeShouldRemoveExistingSubscribers() {
+        StockExchange stockExchange = new StockExchange();
+        stockExchange.addSubscriber(()->{});
+        stockExchange.removeSubscriber(stockExchange.subscribers.get(0));
+
+        assertEquals(0, stockExchange.subscribers.size());
+    }
+
+
+    @Test
+    public void StockExchangeShouldNotRemoveSubscriberIfNotSubscribed() {
+        StockExchange stockExchange = new StockExchange();
+        Subscriber sub = ()->{};
+        stockExchange.addSubscriber(()->{});
+        stockExchange.addSubscriber(sub);
+        stockExchange.removeSubscriber(()->{});
+        stockExchange.removeSubscriber(sub);
+
+        assertEquals(1, stockExchange.subscribers.size());
+    }
 }
