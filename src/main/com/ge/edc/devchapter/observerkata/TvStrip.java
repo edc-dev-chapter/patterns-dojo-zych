e8e6e15d9f95789d4031616ab7d68736b1799786
Przemek Zych <zychster@gmail.com>
2019-03-19 00:03:09 +0100

    Modified subscriber implementations

--- src/main/com/ge/edc/devchapter/observerkata/TvStrip.java
+++ src/main/com/ge/edc/devchapter/observerkata/TvStrip.java
@@ -2,10 +2,12 @@ package com.ge.edc.devchapter.observerkata;
 
 import com.ge.edc.devchapter.observerkata.interfaces.Subscriber;
 
+import java.util.Map;
+
 public class TvStrip implements Subscriber {
 
     @Override
-    public void update() {
+    public void update(Map<String, Double> exchangeRates, Map<String, Double> indices) {
 
     }
 }
