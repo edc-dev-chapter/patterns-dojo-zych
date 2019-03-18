8e67d58dc3127459fbb1b48b4f11554b56114f6a
Przemek Zych <zychster@gmail.com>
2019-03-19 00:45:47 +0100

    Added state to StockExchange

--- src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
+++ src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class StockExchange implements Subject {
-
+    private Map<String, Double> exchangeRates;
+    private Map<String, Double> indices;
     List<Subscriber> subscribers = new ArrayList<>();
 
     @Override
