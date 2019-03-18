c9ee3b7f8e7b2c91bf73514a883c62d7edcc6aba
Przemek Zych <zychster@gmail.com>
2019-03-19 00:38:58 +0100

    Added state to StockExchange - bug fix

--- src/main/com/ge/edc/devchapter/observerkata/SmartphoneApp.java
+++ src/main/com/ge/edc/devchapter/observerkata/SmartphoneApp.java
@@ -3,6 +3,4 @@ package com.ge.edc.devchapter.observerkata;
 import java.util.Map;
 
 public class SmartphoneApp {
-    private Map<String, Double> exchangeRates;
-    private Map<String, Double> indices;
 }
--- src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
+++ src/main/com/ge/edc/devchapter/observerkata/StockExchange.java
@@ -1,4 +1,8 @@
 package com.ge.edc.devchapter.observerkata;
 
+import java.util.Map;
+
 public class StockExchange {
+    private Map<String, Double> exchangeRates;
+    private Map<String, Double> indices;
 }
